a698010da39b265e186c90f2be7cbba2212fa619
dvpesh <dvpesh@mail.ru>
2022-09-03 18:25:36 +0300

    Домашнее задание №7, переделал проект для Spring Data JPA

---
+++ homework7/src/test/java/ru/peshekhonov/Homework7ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.peshekhonov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
